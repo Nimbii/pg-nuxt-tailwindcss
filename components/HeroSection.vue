34a9d003a0c71704f0e64d6d1518bc2c90e8f36c
Akayy <techakayy@gmail.com>
2023-09-22 08:17:10 +1000

    Migration fixes

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -1,61 +1,18 @@
 <script setup lang="ts">
   import { pg_background_urls } from '~~/themes/pg-tailwindcss/tokens.mjs'
 
-  const heroImageSrc =
+  const heroImageUrl =
     pg_background_urls['design-image-large'] ||
     pg_background_urls['design-image']
 
-  const img = useImage()
-  const _srcset = computed(() => {
-    return img.getSizes(heroImageSrc, {
-      sizes: 'xs:100vw sm:100vw md:100vw lg:100vw xl:100vw',
-      densities: 'x1 x2',
-      modifiers: {
-        format: 'webp',
-        quality: 70,
-        height: 800,
-      },
-    })
-  })
-
-  // https://dev.to/ingosteinke/responsive-background-images-with-image-set-the-srcset-for-background-image-259a
-  const responsiveBgImages = computed(() => {
-    return _srcset.value.srcset
-      .split(', ')
-      .filter((imgUrl) => imgUrl.endsWith('768w') || imgUrl.endsWith('2560w'))
-  })
-
-  const responsiveBgImageSrc = computed(() => {
-    return {
-      'background-image': `url("${responsiveBgImages.value[0]}")`,
-    }
-  })
-
-  const responsiveBgImageSrcImageSet = computed(() => {
-    return {
-      'background-image': `image-set(
-      url("${responsiveBgImages.value[0]}") 1x,
-      url("${responsiveBgImages.value[1]}") 2x)`,
-    }
-  })
-
-  const responsiveBgImageSrcImageSetFallback = computed(() => {
-    return {
-      'background-image': `-webkit-image-set(
-      url("${responsiveBgImages.value[0]}") 1x,
-      url("${responsiveBgImages.value[1]}") 2x)`,
-    }
-  })
+  const { optimizeImage } = useOptimizeImage()
+  const { bgStyles } = optimizeImage(heroImageUrl)
 </script>
 <template>
   <section>
     <div
       class="bg-center bg-cover bg-no-repeat blur-none z-0"
-      :style="[
-        responsiveBgImageSrc,
-        responsiveBgImageSrcImageSet,
-        responsiveBgImageSrcImageSetFallback,
-      ]"
+      :style="bgStyles"
     >
       <div
         class="pb-36 pt-2 px-6 relative rounded-3xl md:pb-48 lg:pb-72 lg:px-12"
